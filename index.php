3e3f21053724d0864a9ce024b765c848f5329367
Teddy VALETTE <teddyche@hotmail.Fr>
2017-02-09 14:04:08 +0100

    Création du controleur des utilisateurs et déplacement de la connexion à la BDD

--- index.php
+++ index.php
@@ -3,6 +3,26 @@ session_start();
 require_once ("vendor/autoload.php");
 
 use crazyday\controleurs as controleurs;
+use Illuminate\Database\Capsule\Manager as DB;
+
+//Initialisation de la connexion
+$db = new DB();
+
+$config = parse_ini_file('src/conf/db.ini');
+
+$db->addConnection( [
+    'driver' => $config['driver'],
+    'host' => $config['host'],
+    'database' => $config['database'],
+    'username' => $config['username'],
+    'password' => $config['password'],
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''
+] );
+
+$db->setAsGlobal();
+$db->bootEloquent();
 
 //Création de l'objet du micro-framework
 $app = new Slim\App();
@@ -13,6 +33,7 @@ $app->get('/', function(){
     echo $controleur->renderAccueil();
 })->setName('AcceuilGET');
 
+//Page de liste des utilisateurs
 $app->get('/users', function(){
     echo "test";
 })->setName('UsersGET');
