961c8d5d33466042ad0524a67705c4c80f90958e
Thomas Gig <Thomas Gig>
2017-02-09 18:53:46 +0100

    ajout de style sur les pages de connexions

--- src/crazyday/vues/ConnexionVue.php
+++ src/crazyday/vues/ConnexionVue.php
@@ -7,40 +7,38 @@ class ConnexionVue {
 		$content ="";
 		switch ($i) {
 			case 1: {
+				$racine="";
 				$content = '<form method="POST" action="/connexion">
-					<p> 
-						Nom d\'utilisateur :
-					<input type="text" name="pseudo" />
-						Mot de passe :
-					<input type="password" name="mp"/>
-					<input type="submit" value="Connexion">
-					</br>
-					</p>
-					</form>
-					<p1>
 					
-					</br></br>
-
-					</p1>';
+						<div style="width:230px; display:inline-block;color:#aaaaaa">Votre nom : </div>
+					<input type="text" name="pseudo" /><br /><br />
+						<div style="width:230px; display:inline-block;color:#aaaaaa">Mot de passe : </div>
+					<input type="password" name="mp"/><br /><br />
+					<input class="btn btn-lg btn-default" type="submit" value="Connexion">
+					</br>
+					
+					</form>
+	
+					
+					</br></br>';
 					
 				break;
 			}
 			case 2: {
 				$content = '<form method="POST" action="/connexion">
-					<p> 
-						Nom d\'utilisateur :
-					<input type="text" name="pseudo" />
-						Mot de passe :
+					 <div style="width:230px; display:inline-block;color:#aaaaaa">Votre nom : </div>
+					<input type="text" name="pseudo" /><br /><br />
+						<div style="width:230px; display:inline-block;color:#aaaaaa">Mot de passe: </div>
 					<input type="password" name="mp"/>
-						Erreur mauvais pseudo/mot de passe  
-					<input type="submit" value="Connexion">
-					</p>
+						Erreur mauvais pseudo/mot de passe  <br /><br />
+					<input type="submit" class="btn btn-lg btn-default" value="Connexion">
+					
 					</form>
-					<p1>
-					Vous n\'êtes pas inscrit ? cliquez 
+					
+					<p style="font-size:18px;color:#aaaaaa>"Vous n\'êtes pas inscrit ? cliquez <a href="inscription" style="text-decoration:none">ici</a></p>
 					
 					</br></br>
-					</p1>';
+					';
 					break;
 			}
 			case 3: {
@@ -54,16 +52,61 @@ class ConnexionVue {
 			break;
 			}
 		}
-		$html = '
-<!DOCTYPE html>
-<html>
-<div class="content">
-	'.$content.'
+		$html=<<<END
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Un toit pour tous</title>
+			<link href="{$racine}assets/css/bootstrap.min.css" rel="stylesheet" media="all" type="text/css">
+			<link href="{$racine}assets/css/bootstrap-theme.min.css" rel="stylesheet" media="all" type="text/css">
+			<link href="{$racine}assets/css/plus.css" rel="stylesheet" media="all" type="text/css">
+		</head>
+		<body style="background-color:#333333">
+		
+			<div class="container">
+			<nav class="navbar navbar-inverse">
+			<div class="container">
+			  <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Un toit pour tous</a>
+			  </div>
+			  <div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+				  <li><a href="#">Accueil</a></li>
+				  <li><a href="{$racine}logements">Les logements</a></li>
+				  <li><a href="{$racine}users">Utilisateurs</a></li>
+				  <li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mon profil<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					  <li><a href="#">Page personnelle</a></li>
+					  <li><a href="#">Invitations</a></li>
+					  <li role="separator" class="divider"></li>
+					  <li><a href="#">Deconnexion</a></li>
+					</ul>
+				  </li>
+				</ul>
+			  </div><!--/.nav-collapse -->
+			</div>
+			</nav>
+			
+			$content
+			</div>
+			
+		<footer style="margin-top:100px;">
+			<p align="center" style="color:#aaaaaa">Crazy Charly Day 2017 - Equipe RKO</p>
+		</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="{$racine}assets/js/bootstrap.min.js"></script>
+		</body>
 
-</div>
-</body>
-</html>';
-  
-		return $html;
+		</html>
+END;
+        return $html;
 	}
 }
--- src/crazyday/vues/InscriptionVue.php
+++ src/crazyday/vues/InscriptionVue.php
@@ -5,32 +5,33 @@ class InscriptionVue {
 		$content='';
 		switch($i){
 			case 1: {
+				$racine="";
 				$content='<form method="POST" action="/inscription">
 				Nom d\'utilisateur :';
 						if (empty($tab['pseudo'])){
-							$content.='<input type="text" name="pseudo_inscrit" />pseudo non valide';
+							$content.='<div style="width:230px; display:inline-block;">Votre nom : </div><input type="text" name="pseudo_inscrit" />Nom non valide</br>';
 						}elseif (isset($tab['pseudo'])){
 							$p = $tab['pseudo'];
-							$content.='<input type="text" name="pseudo_inscrit" value='.$p.' />
+							$content.='<div style="width:230px; display:inline-block;">Votre nom : </div><input type="text" name="pseudo_inscrit" value='.$p.' /></br>
 							';
 						}
 						$content.='</div></div>
 						</br>
-						mot de passe: <input type="password" name="mp_inscrit" />
+						<div style="width:230px; display:inline-block;">Mot de passe: </div> <input type="password" name="mp_inscrit" />
 						';
 						if (empty($tab['mp'])){
 							$content.='mot de passe non valide';
 						}
 						$content.='</div>
-						</br>
-						confirmation mot de passe: <input type="password" name="cmp_inscrit" />
+						</br></br>
+						<div style="width:230px; display:inline-block;">Confirmer mot de passe</div><input type="password" name="cmp_inscrit" />
 						</div></div>
 						</br>
 						</br>
 						';
 						
 						$content.='
-						<input type="submit" value="Inscription" />
+						<input class="btn btn-lg btn-default" type="submit" value="Inscription" />
 						
 						Pour retourner à l\'accueil, cliquez <a href="/">ici</a>
 						</form>
@@ -38,9 +39,15 @@ class InscriptionVue {
 						</br>
 						';
 
-						break;
+						
 			}
 			case 2: {
+				$racine="";
+				$content = '<div class="jumbotron" style="background-color:#999999">
+						<h1>Merci de vous etre inscrit</h1>
+						<p>Pour retourner à l\'accueil cliquez 
+							<a href="/" style="text-decoration:none;">ici</a></p>
+						</div>';
 				$content='<p>
 							Merci de vous êtes inscrit.</br>
 							Pour retourner à l\'accueil cliquez 
@@ -49,29 +56,71 @@ class InscriptionVue {
 				break;
 			}
 			case 3: {
+				$racine="";
 				$content='<form method="POST" action="/inscription">';
-				$content.='<p>
-					Nom d\'utilisateur :<input type="text" name="pseudo_inscrit" /></br>
-					mot de passe :<input type="password" name="mp_inscrit" /></br>
-					confirmation mot de passe:<input type="password" name="cmp_inscrit" /></br>
-					<input type="submit" value="Inscription" />
-						';
+				$content.='
+					<div style="width:230px; display:inline-block; color:#aaaaaa">Votre nom : </div><input type="text" name="pseudo_inscrit" /></br></br>
+					<div style="width:230px; display:inline-block; color:#aaaaaa">Mot de passe : </div><input type="password" name="mp_inscrit" /></br></br>
+					<div style="width:230px; display:inline-block; color:#aaaaaa">Confirmer mot de passe :</div><input type="password" name="cmp_inscrit" /></br></br>
+					<input class="btn btn-lg btn-default" type="submit" value="Inscription" />
+						</form>';
 			}
 		}
-		$html = <<<END
-<!DOCTYPE html>
-<html>
-<body>
-
-<div class="well">
-
-		$content
-</body>
-</html>
-
-END;
-
+		$html=<<<END
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Un toit pour tous</title>
+			<link href="{$racine}assets/css/bootstrap.min.css" rel="stylesheet" media="all" type="text/css">
+			<link href="{$racine}assets/css/bootstrap-theme.min.css" rel="stylesheet" media="all" type="text/css">
+			<link href="{$racine}assets/css/plus.css" rel="stylesheet" media="all" type="text/css">
+		</head>
+		<body style="background-color:#333333">
 		
-		return $html;
+			<div class="container">
+			<nav class="navbar navbar-inverse">
+			<div class="container">
+			  <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Un toit pour tous</a>
+			  </div>
+			  <div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+				  <li><a href="#">Accueil</a></li>
+				  <li><a href="{$racine}logements">Les logements</a></li>
+				  <li><a href="{$racine}users">Utilisateurs</a></li>
+				  <li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mon profil<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					  <li><a href="#">Page personnelle</a></li>
+					  <li><a href="#">Invitations</a></li>
+					  <li role="separator" class="divider"></li>
+					  <li><a href="#">Deconnexion</a></li>
+					</ul>
+				  </li>
+				</ul>
+			  </div><!--/.nav-collapse -->
+			</div>
+			</nav>
+			
+			$content
+			</div>
+			
+		<footer style="margin-top:100px;">
+			<p align="center" style="color:#aaaaaa">Crazy Charly Day 2017 - Equipe RKO</p>
+		</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="{$racine}assets/js/bootstrap.min.js"></script>
+		</body>
+
+		</html>
+END;
+        return $html;
 	}
 }
