8b6ed1b7d582e39a39fc2409da948f8ed90131b8
vanvaque1u <x88xs500@gmail.com>
2017-02-09 17:23:25 +0100

    fnejzk

---
+++ src/crazyday/controleurs/InscritpionControleur.php
@@ -0,0 +1,52 @@
+<?php
+namespace crazyday\controleurs;
+use \crazyday\models\User as Utilisateur;
+use \crazyday\vues\InscriptionVue as VueInscription;
+class InscriptionControleur{
+	public function afficher(){
+		$v = new InscriptionVue(); 
+		$v->render(3);
+	}
+
+	public function inscription($pseudo =null,$mp=null,$cmp=null){
+		$pseudo =filter_var($pseudo,FILTER_SANITIZE_STRING);
+		$mp=filter_var($mp,FILTER_SANITIZE_STRING);
+		$cmp=filter_var($cmp,FILTER_SANITIZE_STRING);
+		$email=filter_var($email,FILTER_SANITIZE_EMAIL);
+		$cemail=filter_var($cemail,FILTER_SANITIZE_EMAIL);
+		$tabverite = array();
+		//vérification pseudo
+		$test = User::where('pseudo', '=', $pseudo)->first();
+		if(isset($pseudo) && empty($test) && $pseudo!=null){//rajouter d'autre vérification
+			$tabverite['pseudo'] =$pseudo;
+		}
+		//vérification mot de passe
+		if(isset($mp) && $mp==$cmp && $mp!=null){
+			$tabverite['mp']=$mp;
+		}
+		/*vérification email
+		$test2 = User::where('email', '=', $email)->first();
+		if(isset($email) && $email==$cemail && empty($test2) && $email!=null){
+			$tabverite['email']=$email;
+		}
+		*/
+		$v = new InscriptionVue(); 
+		if (!isset($tabverite['pseudo']) || !isset($tabverite['mp'])){
+			$res=$v->render(1,$tabverite);
+		}else{
+			if (isset($pseudo) && isset($mp) && isset($email)){
+			$n = new Utilisateur();
+			$n->pseudo=$pseudo;
+			$n->mp = $mp; //password_hash($mp,PASSWORD_DEFAULT['cost'=> 12] );
+			$n->save();
+			$res=$v->render(2,$tabverite);
+			$_SESSION['pseudo']=$pseudo;
+			$_SESSION['mp']=$mp;
+		}else{
+			$res=$v->render(1,$tabverite);
+		}
+	}
+		print $res;
+		 
+	}
+}
---
+++ src/crazyday/vues/InscriptionVue.php
@@ -0,0 +1,78 @@
+<?php
+namespace crazyday\vues;
+class InscriptionVue {
+	public function render($i,$tab=null){
+		$content='';
+		switch($i){
+			case 1: {
+
+				$content='';
+
+				$content='<form method="POST" action="/inscription">'
+				
+						
+						if (empty($tab['pseudo'])){
+							$content.='<input type="text" name="pseudo_inscrit" />pseudo non valide';
+						}elseif (isset($tab['pseudo'])){
+							$p = $tab['pseudo'];
+							$content.='<input type="text" name="pseudo_inscrit" value='.$p.' />
+							';
+						}
+						$content.='</div></div>
+						</br>
+						<input type="password" name="mp_inscrit" />
+						';
+						if (empty($tab['mp']){
+							$content.='mot de passe non valide';
+						}
+						$content.='</div>
+						</br>
+						<input type="password" name="cmp_inscrit" />
+						</div></div>
+						</br>
+						</br>
+						';
+						
+						$content.='
+						<input type="submit" value="Inscription" />
+						
+						Pour retourner à l\'accueil, cliquez <a href="/">ici</a>
+						</form>
+						</br>
+						</br>
+						';
+
+						break;
+			}
+			case 2: {
+				$content='<p>
+							Merci de vous êtes inscrit.</br>
+							Pour retourner à l\'accueil cliquez 
+							<a href="/">ici</a>
+							</p>';
+				break;
+			}
+			case 3: {
+				$content='<p>
+					<input type="text" name="pseudo_inscrit" />
+					<input type="password" name="mp_inscrit" />
+					<input type="password" name="cmp_inscrit" />'
+			}
+		}
+		$html = <<<END
+<!DOCTYPE html>
+<html>
+<body>
+
+<div class="well">
+
+		$content
+</body>
+</html>
+
+END;
+
+		
+		return $html;
+	}
+}
