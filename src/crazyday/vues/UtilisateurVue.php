ceb9113772dc1056ee2f8b1cd05f351461c6008e
Teddy VALETTE <teddyche@hotmail.Fr>
2017-02-09 15:23:31 +0100

    Fin de la fonctionnalité 4

--- src/crazyday/vues/UtilisateurVue.php
+++ src/crazyday/vues/UtilisateurVue.php
@@ -16,7 +16,7 @@ class UtilisateurVue
         $html = "<ul>";
 
         foreach ($this->pbc as $users){
-            $html .= "<li><a href='/user/$users->id'>" . $users->nom . " " . $users->message . "<img src='assets/user/$users->id.jpg'/>" . "</a></li>";
+            $html .= "<li><a href='/user/$users->id'>" . $users->nom . " " . $users->message . "<img src='/assets/user/$users->id.jpg'/>" . "</a></li>";
         }
 
         $html .= "</ul>";
@@ -24,11 +24,21 @@ class UtilisateurVue
         return $html;
     }
 
+    private function htmlDetailedUser(){
+        return "<p>ID : " . $this->pbc->id . "; NOM : " . $this->pbc->nom  . "; MESSAGE : " . $this->pbc->message . "</p><img src='/assets/user/" . $this->pbc->id . ".jpg' />";
+    }
+
     function render($selecteur){
 
         switch ($selecteur){
             case "LIST_USERS":
                 $html = $this->htmlListUsers();
+                break;
+            case "DETAILED_USER":
+                $html = $this->htmlDetailedUser();
+                break;
+            case "NO_USER":
+                $html = "<h1>Aucun utilisateur ne correspond à cette URL !</h1>";
                 break;
             default:
                 $html = "<h1>Erreur : mauvais selecteur !!!</h1>";
