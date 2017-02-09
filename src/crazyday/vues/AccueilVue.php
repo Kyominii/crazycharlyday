8f51d1291ea58f60c08e3eab8ce346ca9f0cd91b
vanvaque1u <x88xs500@gmail.com>
2017-02-09 16:28:22 +0100

    nfeui

--- src/crazyday/vues/AccueilVue.php
+++ src/crazyday/vues/AccueilVue.php
@@ -21,7 +21,7 @@ class AccueilVue
 			Connectez vous afin de rejoindre ou créer un groupe de colocation et ainsi trouver votre nouvelle demeur.
 			</p>
             <p class="lead" align="center">
-              <a href=".'$app->urlFor('ConnexionGET').'" class="btn btn-lg btn-default">Connexion</a>
+              <a href="'.$app->urlFor('ConnexionGET').'" class="btn btn-lg btn-default">Connexion</a>
             </p>
           </div>';
     }
